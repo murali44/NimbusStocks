66c58b5abe6513e6719f6a4b20dc71d9a03c664a
Murali Allada <amuralis@hotmail.com>
2017-09-13 00:14:15 +0100

    Automate lambda deployment

--- pkgLambda.sh
+++ pkgLambda.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+echo preparing..
 find . -name "*.pyc" -type f -delete
 rm -rf ~/NimbusStocksEnv
+rm -f ~/NimbusStocks.zip
 
-rm ~/NimbusStocks.zip
-virtualenv ~/NimbusStocksEnv
+echo adding code
+zip -q -rg ~/NimbusStocks.zip *
+
+echo adding dependencies
+virtualenv -q ~/NimbusStocksEnv
 source ~/NimbusStocksEnv/bin/activate
+pip install -q -r requirements.txt
+pushd $VIRTUAL_ENV/lib/python2.7/site-packages > /dev/null #make silent
+zip -q -r9 ~/NimbusStocks.zip *
 
-zip -rg ~/NimbusStocks.zip *
+echo deploying lambda
+aws lambda update-function-code --function-name NimbusStocks --zip-file fileb://~/NimbusStocks.zip --region us-west-2
 
-pip install -r requirements.txt
-pushd $VIRTUAL_ENV/lib/python2.7/site-packages
-zip -r9 ~/NimbusStocks.zip *
-
-popd
+echo cleaning up
+popd > /dev/null #make silent
 rm -rf ~/NimbusStocksEnv
+rm -f ~/NimbusStocks.zip
